4e6f27e998b84e119cf142498cc696aae937058d
Dima Kagan <dimakagan15@gmail.com>
2020-05-27 11:51:20 +0300

    Convert to class

--- age.py
+++ age.py
@@ -23,85 +23,68 @@ except Exception:
     quit()
 
 
-def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
-               font_scale=0.8, thickness=1):
-    size = cv2.getTextSize(label, font, font_scale, thickness)[0]
-    x, y = point
-    cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
-    cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness, lineType=cv2.LINE_AA)
+class AgeEstimator(object):
 
+    def __init__(self, model_path=None, margin=0.4):
+        cfg.freeze()
+        self.model = get_model(model_name=cfg.MODEL.ARCH, pretrained=None)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = self.model.to(device)
 
-def yield_images_from_dir(img_dir):
-    img_dir = Path(img_dir)
+        model_path = Path(model_path)
 
-    for img_path in img_dir.glob("*.*"):
-        try:
-            yield face_recognition.load_image_file(img_path), img_path.name
-        except:
-            pass
+        if model_path is None:
+            model_path = Path(__file__).resolve().parent.joinpath("misc", "epoch044_0.02343_3.9984.pth")
 
+        if not model_path.is_file():
+            print(f"=> model path is not set; start downloading trained model to {model_path}")
+            url = "https://github.com/yu4u/age-estimation-pytorch/releases/download/v1.0/epoch044_0.02343_3.9984.pth"
+            urllib.request.urlretrieve(url, str(model_path))
+            print("=> download finished")
 
-def get_age(img_path, resume_path=None, margin=0.4):
-    cfg.freeze()
+        if model_path.is_file():
+            # print("=> loading checkpoint '{}'".format(model_path))
+            checkpoint = torch.load(model_path, map_location="cpu")
+            self.model.load_state_dict(checkpoint['state_dict'])
+            # print("=> loaded checkpoint '{}'".format(model_path))
+        else:
+            raise ValueError("=> no checkpoint found at '{}'".format(model_path))
 
+        if device == "cuda":
+            cudnn.benchmark = True
 
-    # create model
-    # print("=> creating model '{}'".format(cfg.MODEL.ARCH))
-    model = get_model(model_name=cfg.MODEL.ARCH, pretrained=None)
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = model.to(device)
+        self.model.eval()
 
-    if resume_path is None:
-        resume_path = Path(__file__).resolve().parent.joinpath("misc", "epoch044_0.02343_3.9984.pth")
+    def get_age(self, img):
 
-    if not resume_path.is_file():
-        print(f"=> model path is not set; start downloading trained model to {resume_path}")
-        url = "https://github.com/yu4u/age-estimation-pytorch/releases/download/v1.0/epoch044_0.02343_3.9984.pth"
-        urllib.request.urlretrieve(url, str(resume_path))
-        print("=> download finished")
+        cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
+        detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
 
-    if Path(resume_path).is_file():
-        # print("=> loading checkpoint '{}'".format(resume_path))
-        checkpoint = torch.load(resume_path, map_location="cpu")
-        model.load_state_dict(checkpoint['state_dict'])
-        # print("=> loaded checkpoint '{}'".format(resume_path))
-    else:
-        raise ValueError("=> no checkpoint found at '{}'".format(resume_path))
+        img_size = cfg.MODEL.IMG_SIZE
 
-    if device == "cuda":
-        cudnn.benchmark = True
+        with torch.no_grad():
+            input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_h, img_w, _ = np.shape(input_img)
 
-    model.eval()
+            # detect faces using dlib detector
+            detected = [r.rect for r in detector(input_img, 1)]
 
-    cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
-    detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
+            faces = np.empty((len(detected), img_size, img_size, 3))
 
-    img_size = cfg.MODEL.IMG_SIZE
-    img, name = face_recognition.load_image_file(img_path), Path(img_path).name
+            if len(detected) > 0:
+                for i, d in enumerate(detected):
+                    x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                    xw1 = max(int(x1 - self.margin * w), 0)
+                    yw1 = max(int(y1 - self.margin * h), 0)
+                    xw2 = min(int(x2 + self.margin * w), img_w - 1)
+                    yw2 = min(int(y2 + self.margin * h), img_h - 1)
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
+                    cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+                    faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
 
-    with torch.no_grad():
-        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_h, img_w, _ = np.shape(input_img)
-
-        # detect faces using dlib detector
-        detected = [r.rect for r in detector(input_img, 1)]
-
-        faces = np.empty((len(detected), img_size, img_size, 3))
-
-        if len(detected) > 0:
-            for i, d in enumerate(detected):
-                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
-                xw1 = max(int(x1 - margin * w), 0)
-                yw1 = max(int(y1 - margin * h), 0)
-                xw2 = min(int(x2 + margin * w), img_w - 1)
-                yw2 = min(int(y2 + margin * h), img_h - 1)
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
-                cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
-
-            # predict ages
-            inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
-            outputs = F.softmax(model(inputs), dim=-1).cpu().numpy()
-            ages = np.arange(0, 101)
-            predicted_ages = (outputs * ages).sum(axis=-1)
-            return zip([_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in detected], predicted_ages)
+                # predict ages
+                inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
+                outputs = F.softmax(self.model(inputs), dim=-1).cpu().numpy()
+                ages = np.arange(0, 101)
+                predicted_ages = (outputs * ages).sum(axis=-1)
+                return zip([_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in detected], predicted_ages)
