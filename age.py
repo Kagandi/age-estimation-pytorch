fc9abe4e01c15638635fe3891f946c59bbe864fa
Dima Kagan <dimakagan15@gmail.com>
2020-05-27 13:53:27 +0300

    refactoring

--- age.py
+++ age.py
@@ -60,31 +60,36 @@ class AgeEstimator(object):
         cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
         detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
 
+        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        detected = [r.rect for r in detector(input_img, 1)]
+
+        return self.get_age_from_faces(img, detected)
+
+
+    def get_age_from_faces(self,input_img, bboxs):
+
         img_size = cfg.MODEL.IMG_SIZE
 
         with torch.no_grad():
-            input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img_h, img_w, _ = np.shape(input_img)
 
-            # detect faces using dlib detector
-            detected = [r.rect for r in detector(input_img, 1)]
+            faces = np.empty((len(bboxs), img_size, img_size, 3))
 
-            faces = np.empty((len(detected), img_size, img_size, 3))
-
-            if len(detected) > 0:
-                for i, d in enumerate(detected):
+            if len(bboxs) > 0:
+                for i, d in enumerate(bboxs):
                     x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
                     xw1 = max(int(x1 - self.margin * w), 0)
                     yw1 = max(int(y1 - self.margin * h), 0)
                     xw2 = min(int(x2 + self.margin * w), img_w - 1)
                     yw2 = min(int(y2 + self.margin * h), img_h - 1)
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
-                    cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                    faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
+                    cv2.rectangle(input_img, (x1, y1), (x2, y2), (255, 255, 255), 2)
+                    cv2.rectangle(input_img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+                    faces[i] = cv2.resize(input_img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
 
                 # predict ages
                 inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
                 outputs = F.softmax(self.model(inputs), dim=-1).cpu().numpy()
                 ages = np.arange(0, 101)
                 predicted_ages = (outputs * ages).sum(axis=-1)
-                return zip([_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in detected], predicted_ages)
+                return zip([_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in bboxs], predicted_ages)
