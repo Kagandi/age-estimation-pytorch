dd09da967fa3e6533212f1ab66e4c77c9925cf0a
Dima Kagan <dimakagan15@gmail.com>
2020-05-27 11:15:52 +0300

    Update get_age to rerurn results for only one image

--- age.py
+++ age.py
@@ -39,15 +39,9 @@ def yield_images_from_dir(img_dir):
             pass
 
 
-def get_age(output_dir, img_dir, resume_path=None):
+def get_age(img_path, resume_path=None, margin=0.4):
     cfg.freeze()
 
-    if output_dir is not None:
-        if img_dir is None:
-            raise ValueError("=> --img_dir argument is required if --output_dir is used")
-
-        output_dir = Path(output_dir)
-        output_dir.mkdir(parents=True, exist_ok=True)
 
     # create model
     print("=> creating model '{}'".format(cfg.MODEL.ARCH))
@@ -76,40 +70,37 @@ def get_age(output_dir, img_dir, resume_path=None):
         cudnn.benchmark = True
 
     model.eval()
-    margin = args.margin
-    img_dir = img_dir
 
     cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
     detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
 
     img_size = cfg.MODEL.IMG_SIZE
-    image_generator = yield_images_from_dir(img_dir)
+    img, name = face_recognition.load_image_file(img_path), Path(img_path).name
 
     with torch.no_grad():
-        for img, name in image_generator:
-            input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_h, img_w, _ = np.shape(input_img)
+        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_h, img_w, _ = np.shape(input_img)
 
-            # detect faces using dlib detector
-            detected = [r.rect for r in detector(input_img, 1)]
+        # detect faces using dlib detector
+        detected = [r.rect for r in detector(input_img, 1)]
 
-            faces = np.empty((len(detected), img_size, img_size, 3))
+        faces = np.empty((len(detected), img_size, img_size, 3))
 
-            if len(detected) > 0:
-                for i, d in enumerate(detected):
-                    x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
-                    xw1 = max(int(x1 - margin * w), 0)
-                    yw1 = max(int(y1 - margin * h), 0)
-                    xw2 = min(int(x2 + margin * w), img_w - 1)
-                    yw2 = min(int(y2 + margin * h), img_h - 1)
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
-                    cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                    faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
+        if len(detected) > 0:
+            for i, d in enumerate(detected):
+                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                xw1 = max(int(x1 - margin * w), 0)
+                yw1 = max(int(y1 - margin * h), 0)
+                xw2 = min(int(x2 + margin * w), img_w - 1)
+                yw2 = min(int(y2 + margin * h), img_h - 1)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
+                cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+                faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
 
-                # predict ages
-                inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
-                outputs = F.softmax(model(inputs), dim=-1).cpu().numpy()
-                ages = np.arange(0, 101)
-                predicted_ages = (outputs * ages).sum(axis=-1)
+            # predict ages
+            inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
+            outputs = F.softmax(model(inputs), dim=-1).cpu().numpy()
+            ages = np.arange(0, 101)
+            predicted_ages = (outputs * ages).sum(axis=-1)
 
-                return zip(faces, predicted_ages)
+            return zip(faces, predicted_ages)
