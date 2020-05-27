2e9b98bc402a0779efa4bd96a27410ea4f71f2af
Dima Kagan <dimakagan15@gmail.com>
2020-05-27 11:11:54 +0300

    add init

--- demo.py
+++ demo.py
@@ -23,6 +23,7 @@ except Exception:
     print("pip install git+https://github.com/ageitgey/face_recognition_models")
     quit()
 
+
 def get_args():
     parser = argparse.ArgumentParser(description="Age estimation demo",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -176,89 +177,6 @@ def main():
                     break
 
 
-
-
-def main_dlib(output_dir, img_dir, resume=None):
-    cfg.freeze()
-
-    if output_dir is not None:
-        if img_dir is None:
-            raise ValueError("=> --img_dir argument is required if --output_dir is used")
-
-        output_dir = Path(output_dir)
-        output_dir.mkdir(parents=True, exist_ok=True)
-
-    # create model
-    print("=> creating model '{}'".format(cfg.MODEL.ARCH))
-    model = get_model(model_name=cfg.MODEL.ARCH, pretrained=None)
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = model.to(device)
-
-    # load checkpoint
-    resume_path = resume
-
-    if resume_path is None:
-        resume_path = Path(__file__).resolve().parent.joinpath("misc", "epoch044_0.02343_3.9984.pth")
-
-        if not resume_path.is_file():
-            print(f"=> model path is not set; start downloading trained model to {resume_path}")
-            url = "https://github.com/yu4u/age-estimation-pytorch/releases/download/v1.0/epoch044_0.02343_3.9984.pth"
-            urllib.request.urlretrieve(url, str(resume_path))
-            print("=> download finished")
-
-    if Path(resume_path).is_file():
-        print("=> loading checkpoint '{}'".format(resume_path))
-        checkpoint = torch.load(resume_path, map_location="cpu")
-        model.load_state_dict(checkpoint['state_dict'])
-        print("=> loaded checkpoint '{}'".format(resume_path))
-    else:
-        raise ValueError("=> no checkpoint found at '{}'".format(resume_path))
-
-    if device == "cuda":
-        cudnn.benchmark = True
-
-    model.eval()
-    margin = args.margin
-    img_dir = img_dir
-    detector = dlib.cnn_face_detection_model_v1()
-    img_size = cfg.MODEL.IMG_SIZE
-    image_generator = yield_images_from_dir(img_dir)
-
-    with torch.no_grad():
-        for img, name in image_generator:
-            input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_h, img_w, _ = np.shape(input_img)
-
-            # detect faces using dlib detector
-            detected = detector(input_img, 1)
-            faces = np.empty((len(detected), img_size, img_size, 3))
-
-            if len(detected) > 0:
-                for i, d in enumerate(detected):
-                    x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
-                    xw1 = max(int(x1 - margin * w), 0)
-                    yw1 = max(int(y1 - margin * h), 0)
-                    xw2 = min(int(x2 + margin * w), img_w - 1)
-                    yw2 = min(int(y2 + margin * h), img_h - 1)
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
-                    cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                    faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
-
-                # predict ages
-                inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
-                outputs = F.softmax(model(inputs), dim=-1).cpu().numpy()
-                ages = np.arange(0, 101)
-                predicted_ages = (outputs * ages).sum(axis=-1)
-
-                # draw results
-                for i, d in enumerate(detected):
-                    label = "{}".format(int(predicted_ages[i]))
-                    draw_label(img, (d.left(), d.top()), label)
-
-            output_path = output_dir.joinpath(name)
-            cv2.imwrite(str(output_path), img)
-
-
 def get_age(output_dir, img_dir, faces, resume=None):
     cfg.freeze()
 
@@ -301,7 +219,10 @@ def get_age(output_dir, img_dir, faces, resume=None):
     model.eval()
     margin = args.margin
     img_dir = img_dir
-    detector = dlib.get_frontal_face_detector()
+
+    cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
+    detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
+
     img_size = cfg.MODEL.IMG_SIZE
     image_generator = yield_images_from_dir(img_dir)
 
@@ -311,7 +232,8 @@ def get_age(output_dir, img_dir, faces, resume=None):
             img_h, img_w, _ = np.shape(input_img)
 
             # detect faces using dlib detector
-            detected = detector(input_img, 1)
+            detected = [r.rect for r in detector(input_img, 1)]
+
             faces = np.empty((len(detected), img_size, img_size, 3))
 
             if len(detected) > 0:
@@ -331,13 +253,8 @@ def get_age(output_dir, img_dir, faces, resume=None):
                 ages = np.arange(0, 101)
                 predicted_ages = (outputs * ages).sum(axis=-1)
 
-                # draw results
-                for i, d in enumerate(faces):
-                    label = "{}".format(int(predicted_ages[i]))
-                    draw_label(img, (d.left(), d.top()), label)
+                return zip(faces, predicted_ages)
 
-            output_path = output_dir.joinpath(name)
-            cv2.imwrite(str(output_path), img)
 
 
 if __name__ == '__main__':
